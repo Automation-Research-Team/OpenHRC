4f0c386b6c79d51c5658ab854bcdd205145e2336
Shunki Itadera <s.itadera@aist.go.jp>
2023-01-26 14:51:10 +0900

    add dummy trajectories

--- ohrc_imitation_learning/script/dummy_trajectory_generation.py
+++ ohrc_imitation_learning/script/dummy_trajectory_generation.py
@@ -11,10 +11,13 @@ import os
 
 import cvxpy
 
+from multiprocessing import Pool
+
+
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def getMinJerkTraj_QP(dt, tf, x0, xf):
+def getMinJerkTraj_QP(dt, tf, x0, xf, constraint):
     t = np.arange(0.0, tf, dt)
 
     a = np.zeros((3, 3))
@@ -32,11 +35,11 @@ def getMinJerkTraj_QP(dt, tf, x0, xf):
     N = int(tf/dt)
     Q = np.zeros((9, 9))
     R = np.eye(3)
-    P = np.eye(9)*100.0
+    P = np.eye(9)*1.0e6
     x0_ = x0[["px", "vx", "ax", "py", "vy", "ay",  "pz", "vz", "az"]].values
     xf_ = xf[["px", "vx", "ax", "py", "vy", "ay",  "pz", "vz", "az"]].values
 
-    x, u = use_modeling_tool(A, B, N, Q, R, P, x0_.T, xf_.T)
+    x, u = use_modeling_tool(A, B, N, Q, R, P, x0_.T, xf_.T, constraint)
 
     # t = t[:, np.newaxis]
     xt = np.concatenate(
@@ -53,7 +56,7 @@ def getMinJerkTraj_QP(dt, tf, x0, xf):
     # # print((x))
 
 
-def use_modeling_tool(A, B, N, Q, R, P, x0, xf, umax=None, umin=None, xmin=None, xmax=None):
+def use_modeling_tool(A, B, N, Q, R, P, x0, xf, constraint, umax=None, umin=None, xmin=None, xmax=None):
     """
     solve MPC with modeling tool for test
     """
@@ -71,7 +74,7 @@ def use_modeling_tool(A, B, N, Q, R, P, x0, xf, umax=None, umin=None, xmin=None,
     for t in range(N):
         costlist += 0.5 * cvxpy.quad_form(x[:, t], Q)
         costlist += 0.5 * cvxpy.quad_form(u[:, t], R)
-        costlist += k[t]**2
+        costlist += 1.0e-6*k[t]
 
         constrlist += [x[:, t + 1] == A @ x[:, t] + B @ u[:, t]]
 
@@ -80,7 +83,8 @@ def use_modeling_tool(A, B, N, Q, R, P, x0, xf, umax=None, umin=None, xmin=None,
         if xmax is not None:
             constrlist += [x[:, t] <= xmax[:, 0]]
 
-    costlist += 0.5 * cvxpy.quad_form(x[:, N], P)  # terminal cost
+    costlist += 0.5 * cvxpy.quad_form(x[:, 0]-x0[:, 0], P)  # terminal cost
+    costlist += 0.5 * cvxpy.quad_form(x[:, -1]-xf[:, 0], P)  # terminal cost
     if xmin is not None:
         constrlist += [x[:, N] >= xmin[:, 0]]
     if xmax is not None:
@@ -91,22 +95,21 @@ def use_modeling_tool(A, B, N, Q, R, P, x0, xf, umax=None, umin=None, xmin=None,
     if umin is not None:
         constrlist += [u >= umin]  # input constraints
 
-    constrlist += [x[:, 0] == x0[:, 0]]  # inital state constraints
+    # constrlist += [x[:, 0] == x0[:, 0]]  # inital state constraints
     # constrlist += [x[:, -1] == xf[:, 0]]  # final state constraints
 
-    zh = 0.2
-    dh = 0.05
-    M = 10.0
-    # for t in range(N):
-    # pass
-    # constrlist += [k[0] == 0]
-    # constrlist += [-(x[6, t] - zh) <= M*k[t]]
-    # constrlist += [-(cvxpy.quad_form(x[:, t], np.diag(np.array([1,
-    #                  0, 0, 0, 1, 0, 0, 0, 0]))) - dh) >= -M*(1 - k[t])]
+    zh = constraint["zh"]  # 0.1
+    dh = constraint["dh"]  # 0.05
+    M = 2.0
+    # Big-M method
+    for t in range(N):
+        constrlist += [-(x[6, t] - zh) <= M*k[t]]
+        constrlist += [-(cvxpy.quad_form(x[:, t], np.diag(np.array([1,
+                         0, 0, 1, 0, 0, 0, 0, 0]))) - dh**2) >= -M*(1 - k[t])]
 
     prob = cvxpy.Problem(cvxpy.Minimize(costlist), constrlist)
 
-    prob.solve(verbose=True)
+    prob.solve(verbose=False)
 
     return x.value, u.value
 
@@ -192,149 +195,72 @@ def standardize(x):
     return (x - x.min()) / (x.max() - x.min())
 
 
-if __name__ == "__main__":
-    # t = np.arange(0, 10, 0.1)
-    # nt = np.size(t)
-
-    # xf = np.array([0.0, 0.0, 0.0])
-
-    # p = np.empty((3, 0))
-    # v = np.empty((1, 0))
-
-    tf = 1.0
+def sample_func(i):
+    print("trajectory #: " + str(i))
+    tf = 10.0
     dt = 0.1
+    n = 1
+    x0 = getInitState(n)
+    xf = getFinalState(n)
 
+    xt = []
+
+    tf = (np.random.rand(1) - 0.5)*4.0 + 10.0  # 8.0 ~ 12.0
+    xt.append(getMinJerkTraj(dt, tf, x0, xf))
+
+    A = np.zeros((3, 3))
+    A[0, 1] = 1.0
+    A[1, 2] = 1.0
+
+    B = np.zeros((3, 1))
+    B[2] = 1.0
+
+    zh = [0.1, 0.2]
+    dh = [0.03, 0.1]
+    constraints = []
+    for z in zh:
+        for d in dh:
+            constraint = dict()
+            constraint["zh"] = z
+            constraint["dh"] = d
+            xt.append(getMinJerkTraj_QP(dt, tf, x0, xf, constraint))
+            constraints.append(constraint)
+
+    return xt, constraints
+
+
+def main():
+    N = 100
+
+    initial_num_list = list(range(N))
+    with Pool(processes=os.cpu_count()) as p:
+        results = p.map(
+            func=sample_func, iterable=initial_num_list)
+
+    n_case = len(results[0][0])
     fig = plt.figure()
-    ax = fig.add_subplot(1, 2, 1, projection="3d")
-    ax2 = fig.add_subplot(1, 2, 2, projection="3d")
-    for i in range(100):
-        n = 1
-        x0 = getInitState(n)
-        xf = getFinalState(n)
+    ax = [fig.add_subplot(1, n_case, i+1, projection="3d")
+          for i in range(n_case)]
 
-        tf = (np.random.rand(1) - 0.5)*4.0 + 10.0  # 8.0 ~ 12.0
-        xt = getMinJerkTraj(dt, tf, x0, xf)
+    t = np.arange(0., 2*np.pi, 0.01)
 
-        xt.to_csv(FILE_DIR+"/trajectory/"+str(i+1) +
-                  ".csv", index=None)
+    for i, a in enumerate(results):
+        result, c = a[0], a[1]
+        for j, xt in enumerate(result):
+            ax[j].plot(xt["px"], xt["py"], xt["pz"])
 
-        ax.plot(xt["px"], xt["py"], xt["pz"])
+            if i == len(results)-1:
+                ax[j].axis('equal')
+                if j > 0:
+                    ax[j].plot(c[j-1]["dh"]*np.cos(t), c[j-1]["dh"] *
+                               np.sin(t), c[j-1]["zh"]*np.ones(t.size))
 
-        A = np.zeros((3, 3))
-        A[0, 1] = 1.0
-        A[1, 2] = 1.0
-
-        B = np.zeros((3, 1))
-        B[2] = 1.0
-
-        xt_qp = getMinJerkTraj_QP(dt, tf, x0, xf)
-
-        ax2.plot(xt_qp["px"], xt_qp["py"], xt_qp["pz"])
-
-        # print("x:" + str(np.mean((xt["px"]-xt_qp["px"].values))))
-        # print("y:" + str(np.mean((xt["py"]-xt_qp["py"].values))))
-        # print("z:" + str(np.mean((xt["pz"]-xt_qp["pz"].values))))
-        # ax.plot(x[0, :], x[1, :], x[2, :])
-
+            directory = FILE_DIR+"/trajectory/case_"+str(j)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            xt.to_csv(directory+"/"+str(i+1) + ".csv", index=None)
     plt.show()
 
-    # N = 10000
-    # for i in range(N):
-    #     trj = getTrajectory(t, getInitPos(1), xf)
-    #     p = np.append(p, trj[0], axis=1)
-    #     v = np.append(v, trj[1])
 
-    # R = 0.1
-    # grid_max = np.array([0.5, 0.5, 1.0])
-
-    # grid_min = np.array([-0.5, -0.5, 0.0])
-    # grid_idx = [
-    #     np.floor((p[0, :] - (-grid_min[0])) / R).astype(int),
-    #     np.floor((p[1, :] - (-grid_min[1])) / R).astype(int),
-    #     np.floor((p[2, :] - (-grid_min[2])) / R).astype(int),
-    # ]
-
-    # ix = np.arange(grid_min[0], grid_max[0], R)
-    # iy = np.arange(grid_min[1], grid_max[1], R)
-    # iz = np.arange(grid_min[2], grid_max[2], R)
-    # grid = np.meshgrid(ix, iy, iz)
-
-    # grid_value = np.zeros((np.size(ix), np.size(iy), np.size(iz)))
-    # # l = 0.3
-    # # grid_value = 1.0 - np.exp(
-    # #     -l * np.linalg.norm(np.meshgrid(ix - xf[0], iy - xf[1], iz - xf[2]), axis=0)
-    # # )  #
-    # grid_value_int = np.copy(grid_value)
-    # grid_count = np.zeros((np.size(ix), np.size(iy), np.size(iz))) + 1
-    # grid_appeared = np.zeros((np.size(ix), np.size(iy), np.size(iz)))
-
-    # # update grid map
-    # for i in range(0, len(v)):
-    #     if v[i] > grid_value[grid_idx[0][i], grid_idx[1][i], grid_idx[2][i]]:
-    #         # if (
-    #         #     grid_count[grid_idx[0][i], grid_idx[1][i], grid_idx[2][i]] == 0
-    #         # ):  # init with v[i]
-    #         #     grid_value[grid_idx[0][i], grid_idx[1][i], grid_idx[2][i]] = v[i]
-    #         # else:  # average
-    #         grid_value[grid_idx[0][i], grid_idx[1][i], grid_idx[2][i]] = (
-    #             grid_value[grid_idx[0][i], grid_idx[1][i], grid_idx[2][i]]
-    #             * grid_count[grid_idx[0][i], grid_idx[1][i], grid_idx[2][i]]
-    #             + v[i]
-    #         ) / (grid_count[grid_idx[0][i], grid_idx[1][i], grid_idx[2][i]] + 1.0)
-    #         grid_count[grid_idx[0][i], grid_idx[1][i], grid_idx[2][i]] += 1.0
-    #     grid_appeared[grid_idx[0][i], grid_idx[1][i], grid_idx[2][i]] += 1.0
-
-    # # standalize and mask unfined points
-    # grid_value = standardize(grid_value)
-    # grid_value = np.ma.masked_where(grid_appeared == 0, grid_value)
-
-    # grid_value_int = standardize(grid_value_int)
-    # grid_value_int = np.ma.masked_where(grid_appeared == 0, grid_value_int)
-
-    # # evaluation
-    # Ns = 20
-    # ps = np.empty((3, 0))
-    # vs = np.empty((1, 0))
-    # for i in range(Ns):
-    #     trj = getTrajectory(t, getInitPos(), xf)
-    #     ps = np.append(ps, trj[0], axis=1)
-    #     vs = np.append(vs, trj[1])
-
-    # grids_idx = [
-    #     np.floor((ps[0, :] - (-grid_min[0])) / R).astype(int),
-    #     np.floor((ps[1, :] - (-grid_min[1])) / R).astype(int),
-    #     np.floor((ps[2, :] - (-grid_min[2])) / R).astype(int),
-    # ]
-
-    # grids_value = []
-    # for i in range(0, len(vs)):
-    #     grids_value = np.append(
-    #         grids_value, grid_value[grids_idx[0][i],
-    #                                 grids_idx[1][i], grids_idx[2][i]]
-    #     )
-
-    # # plot
-    # fig = plt.figure()
-    # cmap = "jet"
-
-    # nSubPlot = 3
-    # ax = []
-    # for i in range(nSubPlot):
-    #     ax.append(fig.add_subplot(1, nSubPlot, i + 1, projection="3d"))
-    #     ax[i].set_xlabel("x")
-    #     ax[i].set_ylabel("y")
-    #     ax[i].set_zlabel("z")
-
-    # for i in range(0, 20):
-    #     ax[0].plot(
-    #         p[0, nt * i: nt * (i + 1)],
-    #         p[1, nt * i: nt * (i + 1)],
-    #         p[2, nt * i: nt * (i + 1)],
-    #     )
-    # # ax[0].scatter(grid[0], grid[1], grid[2], s=50.0, c=grid_value_int, cmap=cmap)
-
-    # ax[1].scatter(grid[0], grid[1], grid[2], s=50.0, c=grid_value, cmap=cmap)
-
-    # ax[2].scatter(ps[0, :], ps[1, :], ps[2, :], c=grids_value, cmap=cmap)
-
-    # plt.show()
+if __name__ == "__main__":
+    main()
