97204ae59d94ccff8666f53089369ee27eee2464
Shunki Itadera <s.itadera@aist.go.jp>
2023-04-03 17:24:06 +0900

    update

--- ohrc_chatgpt/src/chatgpt_star_automation.cpp
+++ ohrc_chatgpt/src/chatgpt_star_automation.cpp
@@ -1,9 +1,11 @@
+#include "ohrc_chatgpt/chatgpt_star_automation.hpp"
+
 #include "ohrc_control/single_interface.hpp"
-#include "ohrc_chatgpt/move_end_effector3.hpp"
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "marker_twist_topic_teleoperation");
 
   SingleInterface<MoveEndEffector> interface;
-  if (interface.control() < 0) ROS_ERROR("End by some fails");
+  if (interface.control() < 0)
+    ROS_ERROR("End by some fails");
 }
