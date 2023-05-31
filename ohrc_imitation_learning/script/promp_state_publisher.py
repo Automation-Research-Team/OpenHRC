eb262ab4958e0a47b76fd122c4f76404be86fd36
Shunki Itadera <s.itadera@aist.go.jp>
2023-05-31 19:19:18 +0900

    fix promp publisher

--- ohrc_imitation_learning/script/promp_state_publisher.py
+++ ohrc_imitation_learning/script/promp_state_publisher.py
@@ -43,7 +43,6 @@ pz = [trajs[mode][i]["pz"].values for i in range(100)]
 
 Y = [np.array([[px[i][j], py[i][j], pz[i][j]]
                for j in range(px[i].size)]) for i in range(n_demos)]
-print(type(Y[0]))
 
 dy = trajs[mode][0]["vx"]
 ddy = trajs[mode][0]["ax"]
@@ -52,7 +51,7 @@ ddy = trajs[mode][0]["ax"]
 # T, Y = generate_1d_trajectory_distribution(n_demos, n_steps)
 y_conditional_cov = np.array([0.0000025, 0.0000025, 0.0000025])
 promp = ProMP(n_dims=3, n_weights_per_dim=10)
-promp.imitate(T, Y, verbose=True)
+promp.imitate(T, Y)
 # Y_mean = promp.mean_trajectory(T[0])
 # Y_conf = 1.96 * np.sqrt(promp.var_trajectory(T[0]))
 
@@ -73,41 +72,24 @@ promp.imitate(T, Y, verbose=True)
 # ax2.set_title("Conditional ProMPs")
 
 
-rospy.init_node('talker')  # ノードの生成
+rospy.init_node('promp_state_publisher')  # ノードの生成
 pub = rospy.Publisher('chatter', State, queue_size=1)
-pub_trj = rospy.Publisher('trj', CartesianTrajectory, queue_size=100)
+pub_trj = rospy.Publisher('trj', CartesianTrajectory, queue_size=1)
 rate = rospy.Rate(1)
 # for color, y_cond in zip("rgbcmyk", np.linspace(-0.3, 0.3, 7)):
-y_cond = [0.05, 0.05, 0.3]
+y_cond = [-0.1, 0.1, 0.2]
 while not rospy.is_shutdown():
     cpromp = promp.condition_position(
-        np.array(y_cond), y_cov=y_conditional_cov, t=0, t_max=10.0)
+        np.array(y_cond), y_cov=y_conditional_cov, t=0.0, t_max=12.0)
 
-    t = np.arange(0, 10, 0.1)
-    Y_cmean = cpromp.mean_trajectory(t)
-    # print(Y_cmean.shape)
-    # print(Y_cmean[:, 0])
-    # print(Y_cmean[:, 1])
-    # print(Y_cmean[:, 2])
-    # print("--  -")
-    dY_cmean = cpromp.mean_velocities(t)
-
-    # state = State()
-    # state.pose.position.x = Y_cmean[1, 0]
-    # state.pose.position.y = Y_cmean[1, 1]
-    # state.pose.position.z = Y_cmean[1, 2]
-    # state.twist.linear.x = dY_cmean[1, 0]
-    # state.twist.linear.y = dY_cmean[1, 1]
-    # state.twist.linear.z = dY_cmean[1, 2]
-
-    # y_cond = Y_cmean[1, :]
-
-    # pub.publish(state)
+    time = np.arange(0.0, 12.0, 0.01)
+    Y_cmean = cpromp.mean_trajectory(time)
+    dY_cmean = cpromp.mean_velocities(time)
 
     trj = CartesianTrajectory()
     trj.header.frame_id = "world"
     trj.header.stamp = rospy.Time.now()
-    for i in range(len(t)):
+    for i in range(len(time)):
         point = CartesianTrajectoryPoint()
         point.point.pose.position.x = Y_cmean[i, 0] - y_cond[0]
         point.point.pose.position.y = Y_cmean[i, 1] - y_cond[1]
@@ -115,23 +97,11 @@ while not rospy.is_shutdown():
         point.point.velocity.linear.x = dY_cmean[i, 0]
         point.point.velocity.linear.y = dY_cmean[i, 1]
         point.point.velocity.linear.z = dY_cmean[i, 2]
-        point.time_from_start = rospy.Duration.from_sec(t[i])
+        point.time_from_start = rospy.Duration.from_sec(time[i]*12.0)
         trj.points.append(point)
 
     pub_trj.publish(trj)
     # t += 1.0/10.0
     rate.sleep()
-# Y_cmean = cpromp.mean_trajectory(T[0])
-# Y_cconf = 1.96 * np.sqrt(cpromp.var_trajectory(T[0]))
 
-# ax2.scatter([0], [y_cond], marker="*", s=100,
-#             c=color, label="$y_0 = %.2f$" % y_cond)
-# ax2.fill_between(T[0], (Y_cmean - Y_cconf).ravel(),
-#                  (Y_cmean + Y_cconf).ravel(), color=color, alpha=0.3)
-# ax2.plot(T[0], Y_cmean, c=color, lw=2)
-# ax2.set_xlim((-0.05, 1.05))
-# # ax2.set_ylim((-0.5, 3.5))
-# ax2.legend(loc="best")
-
-# plt.tight_layout()
-# plt.show()
+rospy.spin()
