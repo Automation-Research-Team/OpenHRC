6185307ece8b61785110484163d4d4858d122ba0
Shunki Itadera <s.itadera@aist.go.jp>
2024-03-13 19:37:38 +0900

    modified:   install-docker-rocker.sh

--- install-docker-rocker.sh
+++ install-docker-rocker.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt -y install ca-certificates curl gnupg
+sudo apt upgrade -y
+sudo apt -y install ca-certificates curl gnupg python3-pip
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt -y install docker-ce docker-ce-cli containerd.io
-sudo apt -y install docker-compose-plugin
+sudo apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo gpasswd -a $USER docker
 pip install rocker
